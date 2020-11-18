ce545fa43f58498e208c73b0ffc6607723205d0f
Shucai Xiao <Shucai.Xiao@amd.com>
2020-11-17 21:39:43 -0600

    update scripts to fix issues

--- run_test.sh
+++ run_test.sh
@@ -2,4 +2,6 @@
 . ./tools/download_models.sh
 docker build -t migraphx_test:test -f dockerfile/Dockerfile .
 prj_dir=$(pwd)
-docker run --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v $prj_dir/onnx_models:/workspace/test/onnx_models -v $prj_scripts:/workspace/test/scripts migraphx_test:test /workspace/test/scripts/run_all_models.sh
+cp $prj_dir/list/model_list $prj_dir/onnx_models/.
+docker run --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v=$prj_dir/onnx_models:/workspace/test/onnx_models -v=$prj_dir/scripts:/workspace/test/scripts migraphx_test:test /workspace/test/scripts/run_all_models.sh
+#docker run -it --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v=$prj_dir/onnx_models:/workspace/test/onnx_models -v=$prj_dir/scripts:/workspace/test/scripts migraphx_test:test
--- scripts/run_all_models.sh
+++ scripts/run_all_models.sh
@@ -1,3 +1,4 @@
-now=date +"%Y_%m_%d_%H_%M_%S"
-./onnx_test.sh onnx_models/model_list migraphx 1 |& tee migraphx_iter1_$now
+#!/bin/bash
+now=$(date +"%Y_%m_%d_%H_%M_%S")
+/workspace/test/scripts/onnx_test.sh /workspace/test/onnx_models/model_list migraphx 1 |& tee migraphx_iter1_$now
 
