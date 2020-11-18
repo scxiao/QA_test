ee185e1f88b13d96074e964df22a1aaa60034293
Shucai Xiao <Shucai.Xiao@amd.com>
2020-11-18 14:56:56 +0000

    refine the scenario of downloading onnx models

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
+
+#download model and input/output data if not exits
 . ./tools/download_models.sh
-docker build -t migraphx_test:test -f dockerfile/Dockerfile .
+
+# build a docker image for testing
+#docker build -t migraphx_test:test -f dockerfile/Dockerfile .
+
+# copy the list of models to be executed to the model folder
 prj_dir=$(pwd)
 cp $prj_dir/list/model_list $prj_dir/onnx_models/.
-docker run --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v=$prj_dir/onnx_models:/workspace/test/onnx_models -v=$prj_dir/scripts:/workspace/test/scripts migraphx_test:test /workspace/test/scripts/run_all_models.sh
+
+# create output folder
+mkdir -p $prj_dir/out/
+
+# get the current time and append to the output file name
+now=$(date +"%Y_%m_%d_%H_%M_%S")
+docker run --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v=$prj_dir/onnx_models:/workspace/test/onnx_models -v=$prj_dir/scripts:/workspace/test/scripts migraphx_test:test /workspace/test/scripts/run_all_models.sh |& tee $prj_dir/out/migraphx_1_$now
+
+# an alternative command to run in interactive mode
 #docker run -it --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v=$prj_dir/onnx_models:/workspace/test/onnx_models -v=$prj_dir/scripts:/workspace/test/scripts migraphx_test:test
+
+#remove the onnx_model folder
+rm -r $prj_dir/onnx_models
+
--- tools/download_models.sh
+++ tools/download_models.sh
@@ -6,21 +6,19 @@ then
 fi
 
 model_dir=$ONNX_HOME/onnx_models
-tmp_dir=$ONNX_HOME/tmp
+tmp_dir=$ONNX_HOME/downloaded
 mkdir -p $tmp_dir
-file_name=20191107.zip
+file_name=20191107
+
+# if zip file does not exist, download it
+if [[ ! -f "$tmp_dir/$file_name.zip" ]]; then
+    curl https://onnxruntimetestdata.blob.core.windows.net/models/$file_name.zip --output $tmp_dir/$file_name.zip
+fi
 
 
 if [[ ! -d $model_dir ]]; then
-
-# if zip file does not exist, download it
-if [[ ! -f "$tmp_dir/$file_name" ]]; then
-curl https://onnxruntimetestdata.blob.core.windows.net/models/$file_name --output $tmp_dir/$file_name
-fi
-
-# create the folder and unzip the file
-mkdir -p $model_dir
-unzip $tmp_dir/$file_name -d $model_dir && rm $tmp_dir/$file_name
-
+    # create the folder and unzip the file
+    mkdir -p $model_dir
+    unzip $tmp_dir/$file_name -d $model_dir 
 fi
 
